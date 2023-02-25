99eaebbd0173eb15f368092a2ff756beb24c78b2
shota tada <haru@moremost.jp>
2023-02-25 16:15:24 +0900

    add Custom Search JSON APIを利用するcomposer packageの導入

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        JanDrda\LaravelGoogleCustomSearchEngine\LaravelGoogleCustomSearchEngineProvider::class,
 
         /*
          * Package Service Providers...
@@ -210,6 +211,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'GoogleCseSearch' => JanDrda\LaravelGoogleCustomSearchEngine\LaravelGoogleCustomSearchEngineProvider::class,
     ])->toArray(),
 
 ];
